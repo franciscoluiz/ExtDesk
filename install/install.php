296a79216214c913cf9eb187c389e27c74bbe16e
Xcape <xcape@extdesk.net>
2012-04-20 20:32:39 -0500

    Little changes in install

--- install/install.php
+++ install/install.php
@@ -1,9 +1,16 @@
 <?php	
 
+	sleep(2);
+	
 	include_once ('sql_parse.php');
 
-	$lang     = $_POST['language'];		
+	if(empty($_POST)){
+		die ("{success:false,msg:'Post data don't exist.'}");		
+	}
 	
+	$flag_ini=false;
+	
+	$lang = $_POST['languaje'];		
 	$server1  = $_POST['mysql_server']; 
 	$bd1      = 'mysql';
 	$user1    = $_POST['user'];
@@ -44,12 +51,11 @@
 	$str .= "mysql2[] = \"$pswd2\"\n";
 	$str .= "mysql2[] = \"$schema2\"\n";
 	
-	$ini_file = "config.ini";
-	
-	file_put_contents($ini_file, $str);
+	file_put_contents("config.ini", $str);
  
 	if (!copy('config.ini','../server/include/'.$ini_file)) {
-    	//echo "Archivo Copiado en: $file...\n";
+  		$result=array('success'=>false,'error'=>'0','msg'=>"Config.ini can't be copied");
+		die (json_encode($result));
 	}
 
   if (is_file($ini_file) == TRUE)
@@ -62,13 +68,6 @@
 	$sql_query = @fread(@fopen($sqlfile, 'r'), @filesize($sqlfile));
 	$sql_query = remove_remarks($sql_query);
 	$sql_query = split_sql_file($sql_query, ";");
-	
-	/*$server1 = "127.0.0.1";
-	$bd1 = "mysql";
-	$user1 = "root";
-	$pswd1 = "79513";
-	$schema1 = "extdesk2";
-	 */
 	
 	// we firs conect with mysql default databse;
 	$dsn = "$bd1:dbname=$schema1;host=$server1";
@@ -95,7 +94,7 @@
 	try {
 		$db = new PDO($dsn, $user1, $pswd1);
 	} catch (PDOException $e) {
-    	$result=array('success'=>false,'error'=>'2','msg'=>$e->getMessage());
+    	$result=array('success'=>false,'error'=>'3','msg'=>$e->getMessage());
 		die (json_encode($result));
 	}
 
