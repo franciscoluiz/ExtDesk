749dadc93c0ec0220516671d96215b80c2cbcd88
Xcape <xcape@extdesk.net>
2012-04-20 21:52:55 -0500

    Changes in feedback in installation..

--- install/install.php
+++ install/install.php
@@ -1,6 +1,6 @@
 <?php	
 
-	sleep(2);
+	sleep(0.5);
 	
 	include_once ('sql_parse.php');
 
@@ -8,9 +8,11 @@
 		die ("{success:false,msg:'Post data don't exist.'}");		
 	}
 	
+	
+	
 	$flag_ini=false;
 	
-	$lang = $_POST['languaje'];		
+	$lang 	  = $_POST['language'];		
 	$server1  = $_POST['mysql_server']; 
 	$bd1      = 'mysql';
 	$user1    = $_POST['user'];
@@ -23,18 +25,10 @@
 	$pswd2    = $_POST['password'];
 	$schema2  = $_POST['database'];
 
-	//echo $lang.$server1.$bd1.$user1.$pswd1.$schema1;
-	/*
-	$server2 = "127.0.0.1";	
-	$bd2 = "mysql";
-	$user2 = "root";
-	$pswd2 = "79513";
-	$schema2 = "mysql";
-	*/
-	
+
 	$str  = "[ExtDesk]\n";
-  $str .= "lang=\"$lang\"\n";
-  $str .= "; debug config 1=true, 0=false;\n";
+    $str .= "lang=\"$lang\"\n";
+    $str .= "; debug config 1=true, 0=false;\n";
 	$str .= "debug=0\n";
 	$str .= "[drivers]\n";
 	$str .= ";driver mysql;\n";
@@ -51,6 +45,8 @@
 	$str .= "mysql2[] = \"$pswd2\"\n";
 	$str .= "mysql2[] = \"$schema2\"\n";
 	
+	$ini_file="config.ini";
+	
 	file_put_contents("config.ini", $str);
  
 	if (!copy('config.ini','../server/include/'.$ini_file)) {
@@ -58,21 +54,25 @@
 		die (json_encode($result));
 	}
 
-  if (is_file($ini_file) == TRUE)
-  {
+    if (is_file('../server/include/'.$ini_file) == TRUE)
+    {
       chmod($ini_file, 0666);
       unlink($ini_file);
-  } 
+    } 
   
+
 	$sqlfile   = "installsc.sql";
 	$sql_query = @fread(@fopen($sqlfile, 'r'), @filesize($sqlfile));
 	$sql_query = remove_remarks($sql_query);
 	$sql_query = split_sql_file($sql_query, ";");
 	
+	
 	// we firs conect with mysql default databse;
-	$dsn = "$bd1:dbname=$schema1;host=$server1";
+	$dsn = "$bd1:dbname=mysql;host=$server1";
 	try {
 		$db = new PDO($dsn, $user1, $pswd1);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 	} catch (PDOException $e) {
     	$result=array('success'=>false,'error'=>'2','msg'=>$e->getMessage());
 		die (json_encode($result));
@@ -90,9 +90,13 @@
 	runsql($db,1,"Crear bd","CREATE DATABASE  IF NOT EXISTS $schema1");
 	
 	/*** step 2 ***/
+	echo $dsn;
 	$dsn = "$bd1:dbname=$schema1;host=$server1";
 	try {
 		$db = new PDO($dsn, $user1, $pswd1);
+		$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		
 	} catch (PDOException $e) {
     	$result=array('success'=>false,'error'=>'3','msg'=>$e->getMessage());
 		die (json_encode($result));
